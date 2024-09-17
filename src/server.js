49cb585a8e0394dd0746461ace791d912e431181
anacamorims <ana.camorims.dev@gmail.com>
2024-09-17 14:28:08 -0400

    🧹 cleanup: cleaning code

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
-import { petRouter } from "./routes/findmypet.routes.js"
+import cors from "cors";
+import { petRouter } from "./routes/findmypet.routes.js";
+import connectDB from "./database/connection.db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+const corsConfig = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsConfig));
 app.use(express.json());
 
 app.use(petRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
-
+  connectDB();
+  console.log(`Example app listening on port ${port}`);
+});
