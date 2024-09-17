feae48c518a5abfc60756578e29d527feb4f2ee1
anacamorims <ana.camorims.dev@gmail.com>
2024-09-17 14:30:44 -0400

    ♻️ refactor: refactoring code

--- src/routes/findmypet.routes.js
+++ src/routes/findmypet.routes.js
@@ -1,69 +1,20 @@
 import { Router } from "express";
-import { getAllAnimals, getAllPetLost, getAllPetFound, getAllCat, getAllDog, getPetById, createPet, updatePet, deletePet } from "../controllers/findmypet.controller.js";
+import {
+  getAnimals,
+  getAllPetLost,
+  getAllPetFound,
+  createPet,
+  updatePet,
+  deletePet,
+} from "../controllers/findmypet.controller.js";
 
 const petRouter = Router();
 
-petRouter.get("/animais", (req, res) => {
-    const listPet = getAllAnimals();
-    res.status(200).json({listPet})
-})
+petRouter.get("/animais?", getAnimals);
+petRouter.get("/animais/perdidos", getAllPetLost);
+petRouter.get("/animais/encontrados", getAllPetFound);
+petRouter.post("/animais", createPet);
+petRouter.put("/animais/:id", updatePet);
+petRouter.delete("/animais/:id", deletePet);
 
-petRouter.get("/animais/perdidos", (req, res) => {
-    const listPet = getAllPetLost();
-    res.status(200).json({listPet})
-})
-
-petRouter.get("/animais/encontrados", (req, res) => {
-    const listPet = getAllPetFound();
-    res.status(200).json({listPet})
-})
-
-petRouter.get("/animais/gato", (req, res) => {
-    const listPet = getAllCat();
-    res.status(200).json({listPet})
-})
-
-petRouter.get("/animais/cachorro", (req, res) => {
-    const listPet = getAllDog();
-    res.status(200).json({listPet})
-})
-
-petRouter.get("/animais/:id", (req, res) => {
-    const { id } = req.params;
-    const pet = getPetById(id);
-    if(pet){
-        res.status(200).json(pet)
-    }else{
-        res.status(404).json({ message : "Animal não encontrado" })
-    }
-})
-
-petRouter.post("/animais", (req, res) => {
-    const { name, type, breed, color, age, lastSeenLocation, dateLost, status, description, ownerContact, photoURL } = req.body;
-    const newPet = createPet(name, type, breed, color, age, lastSeenLocation, dateLost, status, description, ownerContact, photoURL);
-    res.status(201).json({ message: newPet.message });
-})
-
-petRouter.put("/animais/:id", (req, res) => {
-    const { name, type, breed, color, age, lastSeenLocation, status, description, ownerContact, photoURL } = req.body;
-    const { id } = req.params;
-    const petUpdate = updatePet(id, name, type, breed, color, age, lastSeenLocation, status, description, ownerContact, photoURL);
-    if(typeof petUpdate === "string"){
-        res.status(404).json({ message: petUpdate.message })
-    }else {
-        res.status(200).json({ message: petUpdate.message })
-    }
-})
-
-petRouter.delete("/animais/:id", (req, res) => {
-    const { id } = req.params;
-    const pet = deletePet(id);
-    if(pet.pet){
-        res.status(200).json({ message: pet.message})
-    }else {
-        res.status(404).json({ message: pet.message })
-    }
-})
-
-
-export { petRouter }
+export { petRouter };
