b7d18904c27f6c4e0a18e12f6430ed97dd921815
anacamorims <ana.camorims.dev@gmail.com>
2024-09-17 14:24:02 -0400

    ♻️ refactor: refactoring code

--- src/controllers/findmypet.controller.js
+++ src/controllers/findmypet.controller.js
@@ -1,60 +1,181 @@
-import { Pet } from "../models/Findmypet.model.js";
-import { listPet } from "../../data/petsData.js";
+import Pet from "../models/Pet.model.js";
 
+export const createPet = async (req, res) => {
+  try {
+    const {
+      name,
+      specie,
+      breed,
+      color,
+      age,
+      lastSeenLocation,
+      dateLost,
+      status,
+      description,
+      ownerContact,
+      photoURL,
+    } = req.body;
+    const newPet = await Pet.create({
+      name,
+      specie,
+      breed,
+      color,
+      age,
+      lastSeenLocation,
+      dateLost,
+      status,
+      description,
+      ownerContact,
+      photoURL,
+    });
+    return res.status(201).json({ newPet });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
-export const getAllAnimals = () => {
-  return listPet
-}
+export const updatePet = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      name,
+      specie,
+      breed,
+      color,
+      age,
+      lastSeenLocation,
+      dateLost,
+      status,
+      description,
+      ownerContact,
+      photoURL,
+    } = req.body;
+    const pet = await Pet.findById(id);
 
-export const getAllPetLost = (status) => {
-  return listPet.filter(({ status }) => status == "Perdido")
-}
-
-export const getAllPetFound = (status) => {
-  return listPet.filter(({ status }) => status == "Encontrado")
-}
-
-export const getAllCat = (type) => {
-  return listPet.filter(({ type }) => type == "Gato")
-}
-
-export const getAllDog = (type) => {
-  return listPet.filter(({ type }) => type == "Cachorro")
-}
-
-export const getPetById = (id) => {
-  return listPet.find(({ id: petId }) => petId == id )
-}
-
-export const createPet = (name, type, breed, color, age, lastSeenLocation, dateLost, status, description, ownerContact,     photoURL) => {
-  const newPet = new Pet (name, type, breed, color, age, lastSeenLocation, dateLost, status, description, ownerContact, photoURL);
-
-  listPet.push(newPet);
-  return { message: "Animal adicionado com sucesso" };
-}
-
-export const updatePet = (id, name, type, breed, color, age, lastSeenLocation, status, description, ownerContact, photoURL)  => {
-
-  const petAlreadyExist = listPet.find(pet => pet.id == id)
-
-  if(petAlreadyExist){
-    let i = listPet.findIndex(pet => pet.id == id)
-    listPet[i] = {
-      ...listPet[i], name, type, breed, color, age, lastSeenLocation, status, description, ownerContact, photoURL
+    if (!pet) {
+      return res.status(404).json({ message: "Animal não encontrado" });
     }
-    return { message:"Dados do animal atualizados com sucesso" }
-  }else{
-    return { message:"Esse animal não existe" }
-  }
-}
 
-export const deletePet = (id) => {
-  const petAlreadyExist = listPet.find(pet => pet.id == id)
-  if(petAlreadyExist){
-    let i = listPet.findIndex(pet => pet.id == id)
-    listPet.splice(i, 1)
-    return { message:"Animal deletado com sucesso.", pet: petAlreadyExist }
-  }else{
-    return { message: "Esse animal não existe" }
+    const updated = await Pet.findByIdAndUpdate(
+      { _id: id },
+      {
+        name,
+        specie,
+        breed,
+        color,
+        age,
+        lastSeenLocation,
+        dateLost,
+        status,
+        description,
+        ownerContact,
+        photoURL,
+      },
+      { new: true }
+    );
+
+    if (updated) {
+      return res
+        .status(200)
+        .json({ message: "Animal atualizado com sucesso " });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Não foi possível atualizar o animal" });
+    }
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({ error: error.message });
   }
-}
+};
+
+export const deletePet = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Pet.findByIdAndDelete({ _id: id });
+
+    if (result) {
+      return res.status(200).json({ message: "Animal deletado com sucesso" });
+    } else {
+      return res.status(404).json({ message: "Animal não encontrado" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ messageError: "Erro ao deletar o animal: " + error.message });
+  }
+};
+
+export const getAnimals = async (req, res) => {
+  const validFields = [
+    "name",
+    "specie",
+    "breed",
+    "color",
+    "age",
+    "lastSeenLocation",
+    "dateLost",
+    "status",
+    "description",
+    "ownerContact",
+    "photoURL",
+  ];
+
+  const query = Object.keys(req.query)
+    .filter((key) => validFields.includes(key))
+    .reduce((obj, key) => {
+      if (typeof req.query[key] === "string") {
+        obj[key] = new RegExp(req.query[key], "i");
+      } else {
+        obj[key] = req.query[key];
+      }
+      return obj;
+    }, {});
+
+  try {
+    const pet = await Pet.find(query);
+
+    if (pet.length > 0) {
+      return res.status(200).json(pet);
+    } else {
+      return res.status(404).json({ message: "Nenhum animal encontrado" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ messageError: "Erro ao buscar os animais: " + error.message });
+  }
+};
+
+export const getAllPetLost = async (req, res) => {
+  try {
+    const pet = await Pet.find({ status: "Perdido" });
+
+    if (pet.length > 0) {
+      return res.status(200).json({ pet });
+    } else {
+      return res.status(404).json({ message: "Nenhum animal encontrado" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ messageError: "Erro ao buscar os animais: " + error.message });
+  }
+};
+
+export const getAllPetFound = async (req, res) => {
+  try {
+    const pet = await Pet.find({ status: "Encontrado" });
+
+    if (pet.length > 0) {
+      return res.status(200).json({ pet });
+    } else {
+      return res.status(404).json({ message: "Nenhum animal encontrado" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ messageError: "Erro ao buscar os animais: " + error.message });
+  }
+};
