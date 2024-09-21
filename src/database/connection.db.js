c5e02e68369d48bb4f816855c7e45c9ef8641747
anacamorims <ana.camorims.dev@gmail.com>
2024-09-20 20:11:15 -0400

    🔧 chore: increasing db connection timeout

--- src/database/connection.db.js
+++ src/database/connection.db.js
@@ -4,7 +4,11 @@ const connectDB = () => {
   console.log("wait connecting to the database");
 
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000, 
+    })
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error));
 };
