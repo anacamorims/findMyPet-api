88f756986feb8976f3986fb4a2543ab870b51411
anacamorims <ana.camorims.dev@gmail.com>
2024-09-20 22:32:10 -0400

    🐛 bug: fixing bug

--- src/database/connection.db.js
+++ src/database/connection.db.js
@@ -1,72 +1,16 @@
 import mongoose from "mongoose";
 
-// Definindo o esquema para o Pet
-const PetSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  specie: {
-    type: String,
-    required: true,
-  },
-  breed: {
-    type: String,
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  lastSeenLocation: {
-    type: String,
-    required: true,
-  },
-  dateLost: {
-    type: Date,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  ownerContact: {
-    type: Number,
-    required: true,
-  },
-  photoURL: {
-    type: String,
-    required: true,
-  },
-});
+const connectDB = () => {
+  console.log("wait connecting to the database");
 
-// Conectando ao banco de dados e registrando o modelo
-const connectDB = async () => {
-  console.log("Aguardando a conexão com o banco de dados");
-
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 70000,
-      socketTimeoutMS: 60000,
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 70000, 
+      socketTimeoutMS: 60000, 
       connectTimeoutMS: 60000,
-    });
-    console.log("MongoDB Atlas Conectado");
-
-    // Registrando o modelo
-    const Pet = mongoose.model("Pet", PetSchema);
-
-    return Pet; // Retorna o modelo para uso posterior
-  } catch (error) {
-    console.error("Erro ao conectar ao MongoDB:", error);
-  }
+    })
+    .then(() => console.log("MongoDB Atlas Connected"))
+    .catch((error) => console.log(error));
 };
 
-export { connectDB, Pet };
+export default connectDB;
